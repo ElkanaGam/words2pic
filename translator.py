38457ae088e2ac332683102be6780f7ff0517650
Elkana Gamliel <40407541+ElkanaGam@users.noreply.github.com>
2019-10-10 13:27:35 +0300

    Update translator.py

--- translator.py
+++ translator.py
@@ -1,2 +1,14 @@
  from googletrans import Translator
  
+ 
+ # get list of hebrew words and return yhe english translte for each one
+ def my_translate (heb_words):
+  #initialize the tranlate module
+  translator = Translator()
+  translated = []
+  for w in heb_words:
+   t = translator.translate(w, dest  = 'en').text
+   translated.append(t)
+  
+  return translated
+   
